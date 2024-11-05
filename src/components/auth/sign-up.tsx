7b2a9f9f0fc6a3bb10ebc475f5aa9d575bab96a5
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-11-05 18:15:23 +0530

    signup component refactored

--- src/components/auth/sign-up.tsx
+++ src/components/auth/sign-up.tsx
@@ -22,18 +22,115 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import GoogleSignInButton from "./signIn-with-google";
 import { PasswordInput } from "./password-input";
 import { SignUpSchema } from "@/lib/validators";
 import AuthProviderWrapper from "./auth-provider-wrapper";
 import { cn } from "@/lib/utils";
 import { MotionDiv } from "../framer-motion";
 import { useState } from "react";
-import { ArrowLeft, CircleArrowLeft } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
+import { toast } from "../ui/use-toast";
+
+import { UseFormReturn } from "react-hook-form";
+
+interface Step1Props {
+  form: UseFormReturn<z.infer<typeof SignUpSchema>>;
+  onNextStep: () => void;
+}
+
+interface Step2Props {
+  form: UseFormReturn<z.infer<typeof SignUpSchema>>;
+  isSubmitting: boolean;
+}
+
+function Step1({ form, onNextStep }: Step1Props) {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onNextStep();
+    }
+  };
+  return (
+    <MotionDiv
+      className="space-y-4"
+      animate={{ translateX: "0%" }}
+      transition={{ ease: "easeInOut" }}
+      onKeyDown={handleKeyDown}
+    >
+      <FormField
+        control={form.control}
+        name="fullName"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Full Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Your Full Name" {...field} />
+            </FormControl>
+            <FormMessage className="text-xs" />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input placeholder="yourname@example.com" {...field} />
+            </FormControl>
+            <FormMessage className="text-xs" />
+          </FormItem>
+        )}
+      />
+      <Button className="w-full" onClick={onNextStep}>
+        Next
+      </Button>
+    </MotionDiv>
+  );
+}
+
+function Step2({ form, isSubmitting }: Step2Props) {
+  return (
+    <MotionDiv
+      className="space-y-4"
+      animate={{ translateX: "0%" }}
+      transition={{ ease: "easeInOut" }}
+    >
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <PasswordInput placeholder="*************" {...field} />
+            </FormControl>
+            <FormMessage className="text-xs" />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="confirmPassword"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Confirm Password</FormLabel>
+            <FormControl>
+              <PasswordInput placeholder="*************" {...field} />
+            </FormControl>
+            <FormMessage className="text-xs" />
+          </FormItem>
+        )}
+      />
+      <Button type="submit" className="w-full" disabled={isSubmitting}>
+        Create Account
+      </Button>
+    </MotionDiv>
+  );
+}
 
 export default function SignUpForm() {
   const [formStep, setFormStep] = useState(0);
-
   const form = useForm<z.infer<typeof SignUpSchema>>({
     resolver: zodResolver(SignUpSchema),
     defaultValues: {
@@ -44,199 +141,62 @@ export default function SignUpForm() {
     },
   });
 
-  let {
-    getValues,
-    formState: { isSubmitting, isDirty, isValid },
-  } = form;
-
-  const nextFormStep = () => {
-    form.trigger(["fullName", "email"]);
-
-    const fullNameState = form.getFieldState("fullName");
-    const emailState = form.getFieldState("email");
-
-    if (!fullNameState.isDirty || fullNameState.invalid) return;
-    if (!emailState.isDirty || emailState.invalid) return;
-
-    setFormStep(1);
+  const nextFormStep = async () => {
+    const isValid = await form.trigger(["fullName", "email"]);
+    if (isValid) {
+      form.clearErrors(); // Clear errors when validation succeeds
+      setFormStep(1);
+    }
   };
 
   const onSubmit = (values: z.infer<typeof SignUpSchema>) => {
     console.log("submitted..", values);
+    toast({
+      title: "Account Created!",
+      description: "Your account has been created successfully.",
+    });
   };
 
   return (
     <div className="box-border py-5 md:pt-3">
       <Card className="mx-auto max-w-sm lg:max-w-md">
-        <div className="flex flex-col ">
-          <CardHeader>
-            <CardTitle className="text-xl text-center">
-              Create an Account
-            </CardTitle>
-            {formStep === 1 && (
-              <CardDescription className="flex space-x-0.5">
-                <ArrowLeft
-                  className="cursor-pointer"
-                  onClick={() => {
-                    setFormStep(0);
-                  }}
-                />
-              </CardDescription>
-            )}
-          </CardHeader>
-        </div>
+        <CardHeader>
+          <CardTitle className="text-xl text-center">
+            Create an Account
+          </CardTitle>
+          {formStep === 1 && (
+            <CardDescription className="flex space-x-0.5">
+              <ArrowLeft
+                aria-label="Go back to the previous step"
+                className="cursor-pointer"
+                onClick={() => setFormStep(0)}
+              />
+            </CardDescription>
+          )}
+        </CardHeader>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="w-full max-w-md lg:max-w-lg flex flex-col gap-4 relative overflow-x-hidden"
+            className="w-full max-w-md lg:max-w-lg flex flex-col gap-4"
           >
             <CardContent>
-              <div className="grid gap-4">
-                <MotionDiv
-                  className={cn("space-y-4", { hidden: formStep == 1 })}
-                  animate={{ translateX: `-${formStep * 100}%` }}
-                  transition={{ ease: "easeInOut" }}
-                >
-                  <div className="grid gap-2">
-                    {/* full name */}
-                    <FormField
-                      control={form.control}
-                      name="fullName"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Full Name</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Kondeti Shivaji" {...field} />
-                          </FormControl>
-                          <div className="h-1 ">
-                            <FormMessage className="text-xs" />
-                          </div>
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-
-                  <div className="grid gap-2">
-                    {/* email */}
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Email</FormLabel>
-                          <FormControl>
-                            <Input placeholder="vy@gmail.com" {...field} />
-                          </FormControl>
-                          <div className="h-1 ">
-                            <FormMessage className="text-xs" />
-                          </div>
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </MotionDiv>
-
-                <MotionDiv
-                  className={cn("space-y-4", {
-                    hidden: formStep == 0,
-                  })}
-                  //formstep == 0 ->translateX == 100%
-                  //formstep == 1 ->translateX == 0%
-
-                  animate={{ translateX: `-${100 - formStep * 100}%` }}
-                  transition={{ ease: "easeInOut" }}
-                >
-                  <div className="grid gap-2">
-                    {/* password */}
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password</FormLabel>
-                          <FormControl>
-                            <div className="flex w-full">
-                              <PasswordInput
-                                placeholder="*************"
-                                {...field}
-                              />
-                            </div>
-                          </FormControl>
-                          <div className="h-1">
-                            <FormMessage className="text-xs" />
-                          </div>
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    {/* confirm password */}
-                    <FormField
-                      control={form.control}
-                      name="confirmPassword"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Confirm Password</FormLabel>
-                          <FormControl>
-                            <div className="flex w-full">
-                              <PasswordInput
-                                placeholder="*************"
-                                {...field}
-                              />
-                            </div>
-                          </FormControl>
-                          <div className="h-1">
-                            <FormMessage className="text-xs" />
-                          </div>
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </MotionDiv>
-
-                {/* submit button */}
-                <div>
-                  {/*  */}
-                  <div className="mt-2 flex space-x-6 justify-around">
-                    {formStep == 0 && (
-                      <Button className="w-full" onClick={nextFormStep}>
-                        Next
-                      </Button>
-                    )}
-
-                    {formStep == 1 && (
-                      <Button
-                        type="submit"
-                        className="w-full mb-0.5"
-                        disabled={isSubmitting}
-                      >
-                        Create Account
-                      </Button>
-                    )}
-                  </div>
-                  {/*  */}
-
-                  <div className="text-sm font-normal mx-1 mt-2">
-                    <p>
-                      By clicking this you agree to our{" "}
-                      <span className="text-blue-600">Terms & Conditions</span>{" "}
-                      & <span className="text-blue-600">Privacy policy</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
+              {formStep === 0 && (
+                <Step1 form={form} onNextStep={nextFormStep} />
+              )}
+              {formStep === 1 && (
+                <Step2 form={form} isSubmitting={form.formState.isSubmitting} />
+              )}
             </CardContent>
           </form>
-
           <CardFooter className="flex-col">
-            <div className="mx-auto mb-3 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+            <div className="mx-auto mb-3 flex w-full items-center justify-evenly before:mr-4 before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:h-px after:flex-grow after:bg-stone-400">
               or
             </div>
             <AuthProviderWrapper />
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?
-              <Link href="/sign-up" className="underline">
-                Sign up
+              Already have an account?
+              <Link href="/login" className="underline ml-1">
+                Sign in
               </Link>
             </div>
           </CardFooter>
