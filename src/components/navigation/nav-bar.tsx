aa7ec638ea5638b6c0eb8df577ce1891e405821a
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-11-15 22:06:57 +0530

    icons bugs

--- src/components/navigation/nav-bar.tsx
+++ src/components/navigation/nav-bar.tsx
@@ -5,6 +5,7 @@ import { Header } from "./nav-wrapper";
 import { NavLinks } from "@/components/navigation/nav-links";
 import BagCount from "@/components/bag/bag-count";
 import BagIcon from "@/components/icons/bag-icon";
+import { cn } from "@/lib/utils";
 
 const redressed = localFont({
   src: "../../fonts/Redressed-Regular.ttf",
@@ -24,7 +25,20 @@ const NavBar = () => {
         <div className="flex space-x-6">
           <div className="relative">
             <Link href="/bag">
-              <BagIcon />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-7 w-7 cursor-pointer dark:text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                />
+              </svg>
               <BagCount />
             </Link>
           </div>
