10d904d62c75faf49e49e833d3638780ef29d75b
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-11-27 02:28:41 +0530

    middleware added to protect the pages from authorized users

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import {
-  PUBLIC_ROUTES,
   AUTH_ROUTES,
-  API_AUTH_PREFIX,
   PROTECTED_ROUTES,
   DEFAULT_LOGIN_REDIRECT,
 } from "./routes";
-import { getServerSession } from "./auth/getServerSession";
 
-import { auth } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  const user = await getServerSession();
-
-  const { sessionId } = await auth();
-
-  console.log("session", sessionId);
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+  const { pathname } = req.nextUrl;
 
   const isProtectedRoute = PROTECTED_ROUTES.includes(pathname);
   const isAuthRoute = AUTH_ROUTES.includes(pathname);
 
-  if (isProtectedRoute && !sessionId) {
-    return NextResponse.redirect(new URL("/sign-in", request.nextUrl));
+  if (isProtectedRoute && !userId) {
+    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }
 
-  if (sessionId && isAuthRoute) {
-    return NextResponse.redirect(
-      new URL(DEFAULT_LOGIN_REDIRECT, request.nextUrl)
-    );
+  if (userId && isAuthRoute) {
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
   }
-
-  return NextResponse.next();
-}
+});
 
 export const config = {
   matcher: [
-    // Match all routes except "/", "/product", "/bag", public files, and API routes
+    // Match all routes except "/", "/product", "/bag", public files, and API routes. middlware runs for auth and protected routes
     "/((?!_next|api|trpc|bag|product|$|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
