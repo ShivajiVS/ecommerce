a6808722e135d53d12b26f6ca34d8452551320cd
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-12-03 23:19:25 +0530

    loading file added for bag page

--- src/app/(Frontend)/test/page.tsx
+++ src/app/(Frontend)/test/page.tsx
@@ -81,7 +81,17 @@ import { Fragment } from "react";
 
 export default async function Page() {
   return (
-    <div>
+    <div className="relative h-[500vh]">
+      <div className="">
+        <img
+          className="w-screen h-full absolute top-0"
+          src="https://img.freepik.com/free-vector/abstract-horizontal-grid-lines-graph-style-graphic-design_1017-39918.jpg?t=st=1733076845~exp=1733080445~hmac=b6ff8a3bd1fadcde3664b3a4c7a661efbfc7605dbb20a958f768754865f176ee&w=1380"
+          alt=""
+        />
+      </div>
+      <div className="">
+
+      </div>
       <div className="w-full h-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-5 md:gap-5 py-1">
         {Array(8)
           .fill(0)
