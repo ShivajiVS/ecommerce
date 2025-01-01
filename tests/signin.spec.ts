492d6fbe64290d182ef60b4598b167d311a0a6c9
Kondeti Sivaji <shivajikondeti555@gmail.com>
2025-01-01 22:33:06 +0530

    signin test code updated

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test("should render the sign-in page correctly", async ({ page }) => {
   await page.goto("http://localhost:3000/sign-in");
 
-  const heading = await page.getByRole("heading", { name: "Login" });
+  const heading = page.getByRole("heading", { name: "Login" });
 
   await expect(heading).toBeVisible();
 });
@@ -12,25 +12,27 @@ test("should have a visible email and password input fields", async ({
   page,
 }) => {
   await page.goto("http://localhost:3000/sign-in");
-  const email = await page.getByRole("textbox", { name: "Email" });
+  const email = page.getByLabel("email");
   await expect(email).toBeVisible();
 
-  const password = await page.getByRole("textbox", { name: "*************" });
+  const password = page.getByLabel("password");
   await expect(password).toBeVisible();
+
 });
 
 test("should disable the Sign In button by default", async ({ page }) => {
   await page.goto("http://localhost:3000/sign-in");
-  const loginButton = await page.getByRole("button", { name: "Login" });
+
+  const loginButton = page.getByRole("button", { name: "Login" });
   await expect(loginButton).not.toBeDisabled();
 });
 
 test('should display a "Forgot Password?" link', async ({ page }) => {
   await page.goto("http://localhost:3000/sign-in");
 
-  const forgotButton = await page.getByRole("link", {
+  const forgotButton = page.getByRole("link", {
     name: "Forgot your password",
   });
 
   await expect(forgotButton).toBeVisible();
-})
+});
