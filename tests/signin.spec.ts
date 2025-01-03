4059d8a54d53f5f085608e163b2c3b6afc345b7e
Kondeti Sivaji <shivajikondeti555@gmail.com>
2025-01-03 17:44:15 +0530

    veifying sign-in functionlity

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -1,55 +1,50 @@
-
 import { test, expect } from "@playwright/test";
 
-const SIGN_IN_URL = "http://localhost:3000/sign-in";
+import { clerk, clerkSetup } from "@clerk/testing/playwright";
 
-test.describe("sign-in page UI testing", ()=>{
+test.describe("sign-in page UI testing", () => {
+  test("should render the sign-in page correctly", async ({ page }) => {
+    await page.goto("http://localhost:3000/sign-in");
 
+    const heading = page.getByRole("heading", { name: "Login" });
 
-      test("should render the sign-in page correctly", async ({ page }) => {
-      await page.goto(SIGN_IN_URL);
+    await expect(heading).toBeVisible();
+  });
 
-      const heading = page.getByRole("heading", { name: "Login" });
+  test("should have a visible email and password input fields", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/sign-in");
+    const email = page.getByTestId("email");
+    await expect(email).toBeVisible();
 
-      await expect(heading).toBeVisible();
+    const password = page.getByTestId("password");
+    await expect(password).toBeVisible();
+  });
+
+  test("should disable the Sign In button by default", async ({ page }) => {
+    await page.goto("http://localhost:3000/sign-in");
+
+    const loginButton = page.getByRole("button", { name: "Login" });
+    await expect(loginButton).not.toBeDisabled();
+  });
+
+  test('should display a "Forgot Password?" link', async ({ page }) => {
+    await page.goto("http://localhost:3000/sign-in");
+
+    const forgotButton = page.getByRole("link", {
+      name: "Forgot your password",
     });
 
-    test("should have a visible email and password input fields", async ({
-      page,
-    }) => {
-      await page.goto(`${SIGN_IN_URL}`)
-      const email = page.getByTestId("email");
-      await expect(email).toBeVisible();
+    await expect(forgotButton).toBeVisible();
+  });
+});
 
-      const password = page.getByTestId("password");
-      await expect(password).toBeVisible();
-    });
-
-    test("should disable the Sign In button by default", async ({ page }) => {
-      await page.goto(${SIGN_IN_UR});
-
-      const loginButton = page.getByRole("button", { name: "Login" });
-      await expect(loginButton).not.toBeDisabled();
-    });
-
-    test('should display a "Forgot Password?" link', async ({ page }) => {
-      await page.goto(${SIGN_IN_UR});
-
-      const forgotButton = page.getByRole("link", {
-        name: "Forgot your password",
-      });
-
-      await expect(forgotButton).toBeVisible();
-    })
-})
-
-
-test.describe("sign-in page functionality testing", ()=>{
-
+test.describe("sign-in page functionality testing", () => {
   test("should display an errors for empty email and password", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     await page.getByRole("button", { name: "Login" }).click();
 
@@ -66,10 +61,8 @@ test.describe("sign-in page functionality testing", ()=>{
     expect(passwordErrorMessage?.trim()).toBe("required");
   });
 
-  test("should display an error for invalid email format", async ({
-    page,
-  }) => {
-    await page.goto(${SIGN_IN_UR});
+  test("should display an error for invalid email format", async ({ page }) => {
+    await page.goto("http://localhost:3000/sign-in");
 
     const email = page.getByTestId("email");
     await email.fill("shivaji#gmail.com");
@@ -86,7 +79,7 @@ test.describe("sign-in page functionality testing", ()=>{
   test("should display an error for less than 8 characters of password", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     const password = page.getByTestId("password");
     await password.fill("111");
@@ -97,13 +90,15 @@ test.describe("sign-in page functionality testing", ()=>{
       .getByTestId("passwordErrorMessage")
       .textContent();
 
-    await expect(passwordErrorMessage?.trim()).toBe("Password must be at least 8 characters long.");
+    await expect(passwordErrorMessage?.trim()).toBe(
+      "Password must be at least 8 characters long."
+    );
   });
 
   test("should display an error for more than 8 characters of password but without any digits", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     const password = page.getByTestId("password");
     await password.fill("Test@jhd");
@@ -114,16 +109,18 @@ test.describe("sign-in page functionality testing", ()=>{
       .getByTestId("passwordErrorMessage")
       .textContent();
 
-    await expect(passwordErrorMessage?.trim()).toBe("Password must include at least one number.")
+    await expect(passwordErrorMessage?.trim()).toBe(
+      "Password must include at least one number."
+    );
   });
 
   test("should display an error for more than 8 characters of password but without any lowecase letters", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     const password = page.getByTestId("password");
-    await password.fill("1111111");
+    await password.fill("11111111");
 
     await page.getByRole("button", { name: "Login" }).click();
 
@@ -131,13 +128,15 @@ test.describe("sign-in page functionality testing", ()=>{
       .getByTestId("passwordErrorMessage")
       .textContent();
 
-    await expect(passwordErrorMessage?.trim()).toBe("Password must include at least one lowercase letter.");
+    await expect(passwordErrorMessage?.trim()).toBe(
+      "Password must include at least one lowercase letter."
+    );
   });
 
   test("should display an error for more than 8 characters of password but without any uppercase letters", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     const password = page.getByTestId("password");
     await password.fill("1111111s");
@@ -148,13 +147,15 @@ test.describe("sign-in page functionality testing", ()=>{
       .getByTestId("passwordErrorMessage")
       .textContent();
 
-    await expect(passwordErrorMessage?.trim()).toBe("Password must include at least one uppercase letter.");
+    await expect(passwordErrorMessage?.trim()).toBe(
+      "Password must include at least one uppercase letter."
+    );
   });
 
   test("should display an error for but without any uppercase letters", async ({
     page,
   }) => {
-    await page.goto(${SIGN_IN_UR});
+    await page.goto("http://localhost:3000/sign-in");
 
     const password = page.getByTestId("password");
     await password.fill("1111111sS");
@@ -165,65 +166,72 @@ test.describe("sign-in page functionality testing", ()=>{
       .getByTestId("passwordErrorMessage")
       .textContent();
 
-    await expect(passwordErrorMessage?.trim()).toBe("Password must include at least one special character (e.g.@, #, $,).");
-  })
+    await expect(passwordErrorMessage?.trim()).toBe(
+      "Password must include at least one special character (e.g.@, #, $,)."
+    );
+  });
 
-  test("should display an error message for invalid email(email doesn't exist on the db )", async ({
-      page,
-    }) => {
-      await page.goto(${SIGN_IN_UR});
+  test("should display an error message for invalid email(doesn't exist on the db )", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/sign-in");
 
-        const email = page.getByTestId("email");
-        await email.fill("shivaji@gmail.com");
+    const email = page.getByTestId("email");
+    await email.fill("shivaji@gmail.com");
 
-        const password = page.getByTestId("password");
-        await password.fill("Test12@#");
+    const password = page.getByTestId("password");
+    await password.fill("Test12@#");
 
-      await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("button", { name: "Login" }).click();
 
-      const formErrorMessage = await page
-        .getByTestId("formErrorMessage")
-        .textContent();
+    const formErrorMessage = await page
+      .getByTestId("formErrorMessage")
+      .textContent();
 
-      await expect(formErrorMessage?.trim()).toBe("Invalid email.");
+    await expect(formErrorMessage?.trim()).toBe("Couldn't find your account.");
   });
 
   test("should display an error message for invalid password(password doesn't exist on the db)", async ({
-      page,
-    }) => {
-      await page.goto(${SIGN_IN_UR});
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/sign-in");
 
-        const email = page.getByTestId("email");
-        await email.fill("shivaji@gmail.com");
+    const email = page.getByTestId("email");
+    await email.fill("shivaji@gmail.com");
 
-        const password = page.getByTestId("password");
-        await password.fill("Test12@#");
+    const password = page.getByTestId("password");
+    await password.fill("Test12@#");
 
-      await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("button", { name: "Login" }).click();
 
-      const formErrorMessage = await page
-        .getByTestId("formErrorMessage")
-        .textContent();
+    const formErrorMessage = await page
+      .getByTestId("formErrorMessage")
+      .textContent();
 
-      await expect(formErrorMessage?.trim()).toBe("Invalid email.");
+    expect(formErrorMessage?.trim()).toBe(
+      "Password is incorrect. Try again, or use another method."
+    );
   });
 
-  test("should display home page with text :'best selling products' after completion of sucessfull sign-in", async ({
-      page,
-    }) => {
-      await page.goto(${SIGN_IN_UR});
+  test("should display home page with text :'best selling products' after successful sign-in", async ({
+    page,
+  }) => {
+    await page.goto("https://ecommerce-vsy.vercel.app/sign-in");
 
-        const email = page.getByTestId("email");
-        await email.fill("shivaji@gmail.com");
+    const email = page.getByTestId("email");
+    await email.fill("sivajikondeti40@gmail.com");
 
-        const password = page.getByTestId("password");
-        await password.fill("Test12@#");
+    const password = page.getByTestId("password");
+    await password.fill("Shivaji12@#");
 
-        await page.getByRole("button", { name: "Login" }).click();
+    console.log("Attempting to login...");
+    await page.getByRole("button", { name: "Login" }).click();
 
-        await expect(page).toHaveURL("/")
+    await page.waitForURL("https://ecommerce-vsy.vercel.app/", {
+      timeout: 10000,
+    });
 
-        await expect(page).toHaveText("best selling products").toBeVisible()
-  })
-
-})
+    const text = page.getByText("best selling products");
+    await expect(text).toBeVisible();
+  });
+});
