e9a20580182d8744dd4208dc217e4031fda4821e
Kondeti Sivaji <shivajikondeti555@gmail.com>
2025-01-10 20:15:35 +0530

    order history page loading UI updated

--- src/app/(Frontend)/(ProtectedRoutes)/orders/loading.tsx
+++ src/app/(Frontend)/(ProtectedRoutes)/orders/loading.tsx
@@ -1,30 +1,45 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { Fragment } from "react";
 
 export default function Loading() {
   return (
-    <div className="h-[calc(100vh-100px)] w-full flex flex-col items-center justify-center">
-      <Skeleton className="flex flex-col">
-        {Array(2)
-          .fill(0)
-          .map((_, idx) => (
-            <Fragment key={idx}>
-              <div className="flex space-x-5 items-center">
-                <Skeleton className="h-3 w-40" />
-                <Skeleton className="h-3 w-40" />
-                <Skeleton className="h-3 w-40" />
-              </div>
-              <div>
-                <Skeleton className="h-32 w-32" />
-                <div className="space-y-3">
-                  <Skeleton className="h-2 w-60" />
-                  <Skeleton className="h-2 w-20" />
-                  <Skeleton className="h-2 w-20" />
+    <div className="h-[calc(100vh-100px)] w-full max-w-4xl mx-2 lg:mx-auto mt-4 relative">
+      <div className="container mx-auto p-4">
+        <div className="lg:text-center">
+          <h2 className="text-2xl font-extrabold tracking-tight">
+            Order history
+          </h2>
+
+          <h4 className="text-muted-foreground text-sm tracking-tight font-medium mt-1">
+            Check the status of your recent and old orders
+          </h4>
+        </div>
+
+        <div className="space-y-5 mt-6 mb-8">
+          {/* Order Card Skeleton */}
+          {[...Array(4)].map((_, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between p-4 border rounded-lg shadow-sm"
+            >
+              <div className="flex space-x-4">
+                {/* Image Skeleton */}
+                <Skeleton className="w-20 h-20 rounded-md" />
+                <div className="space-y-2">
+                  {/* Title Skeleton */}
+                  <Skeleton className="w-48 h-6" />
+                  {/* Details Skeleton */}
+                  <Skeleton className="w-32 h-4" />
+                  <Skeleton className="w-40 h-4" />
                 </div>
               </div>
-            </Fragment>
+              <div>
+                {/* Button Skeleton */}
+                <Skeleton className="w-24 h-8 rounded" />
+              </div>
+            </div>
           ))}
-      </Skeleton>
+        </div>
+      </div>
     </div>
   );
 }
