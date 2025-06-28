ca4507e0973340c4bbdd9dffd4eb58ab08c3ec56
kondeti shivaji <shivajikondeti555@gmail.com>
2025-06-28 18:12:49 +0530

    load bag component dynamically

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   eslint: {
     ignoreDuringBuilds: true, // Add if ESLint is causing issues
   },
